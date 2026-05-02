9e3457b4cdc13069e618bbc73c58dd8f2ee869d8
Ridvan Chasan <36571711+rich-info@users.noreply.github.com>
2026-05-02 12:13:12 +0200

    Modify database migration logging check-db

--- scripts/check-db.js
+++ scripts/check-db.js
@@ -69,7 +69,8 @@ async function applyMigration() {
   if (!process.env.SKIP_DB_MIGRATION) {
     //console.log(execSync('prisma migrate deploy').toString());
     const directUrl = process.env.DIRECT_DATABASE_URL || process.env.DATABASE_URL;
-    console.log(execSync('prisma migrate deploy', { env: { ...process.env, DATABASE_URL: directUrl } }).toString());
+    //console.log(execSync('prisma migrate deploy', { env: { ...process.env, DATABASE_URL: directUrl } }).toString());
+    console.log(execSync('prisma migrate deploy', { env: { ...process.env, DATABASE_URL: directUrl, DIRECT_DATABASE_URL: undefined } }).toString());
 
     success('Database is up to date.');
   }
